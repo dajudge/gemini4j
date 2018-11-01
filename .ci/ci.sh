73ca43469e076e216fe706def7fcf23d2d239be4
Alex Stockinger <mail@alexstockinger.de>
2018-11-01 07:12:31 +0000

    Removed DIND caching - not worth it

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -9,16 +9,6 @@ else
     DOCKER_HOST_VAR=DOCKER_HOST
 fi
 
-DIND_CACHE=$CI_PROJECT_DIR/.cache/.dind
-mkdir -p $DIND_CACHE
-echo "Docker image cache: $DIND_CACHE"
-
-for filename in $DIND_CACHE/*.tar; do
-    [ -e "$filename" ] || continue
-    echo "Loading cached docker image '$filename'..."
-    docker load -i "$filename"
-done
-
 docker build -t builder_jdk8 builder/jdk8
 docker run --rm \
     --net host \
@@ -29,12 +19,3 @@ docker run --rm \
     --workdir /project \
     builder_jdk8 \
     $@
-
-docker images
-for i in $(cat $CI_PROJECT_DIR/.ci/dind-cache.config); do
-    escaped=`echo "$i" | sed -r 's/\\//___/g'`
-    filename="$DIND_CACHE/${escaped}.tar"
-    [ -e "$filename" ] && echo "Cached image $filename already exists." && continue
-    echo "Caching image $i..."
-    docker save -o "$filename" "${i}"
-done

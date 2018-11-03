876a0f6edf450c00c8b7897cec5906b199b02ee0
Alex Stockinger <mail@alexstockinger.de>
2018-11-03 12:38:25 +0000

    Fixing CI build

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -1,14 +1,13 @@
 #! /bin/sh
 
-echo "DOCKER_HOST: ${DOCKER_HOST}"
 echo "CI_PROJECT_DIR: ${CI_PROJECT_DIR}"
 
 docker build -t builder_jdk8 builder/jdk8
 docker run --rm \
     --net host \
-    -e DOCKER_HOST="${DOCKER_HOST}" \
     -v "${CI_PROJECT_DIR}":/project \
     -v "${CI_PROJECT_DIR}/.cache/.gradle":/root/.gradle \
+    -v /var/run/docker.sock:/var/run/docker.sock \
     --workdir /project \
     builder_jdk8 \
     $@

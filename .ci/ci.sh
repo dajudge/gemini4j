dc9a391ba018560ded01413a65199370e965dc03
Alex Stockinger <mail@alexstockinger.de>
2018-11-03 07:02:00 +0000

    Added sonar integration

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -1,18 +1,9 @@
 #! /bin/sh
 
-echo "Running gemini4j build in $CI_PROJECT_DIR..."
-echo "DOCKER_HOST: $DOCKER_HOST"
-
-if [ -z "$var" ]; then
-    DOCKER_HOST_VAR=DISABLED_DOCKER_HOST
-else
-    DOCKER_HOST_VAR=DOCKER_HOST
-fi
-
 docker build -t builder_jdk8 builder/jdk8
 docker run --rm \
     --net host \
-    -e $DOCKER_HOST_VAR=$DOCKER_HOST \
+    -e DOCKER_HOST=$DOCKER_HOST \
     -v "$CI_PROJECT_DIR":/project \
     -v $(pwd)/.cache/.gradle:/root/.gradle \
     -v /var/run/docker.sock:/var/run/docker.sock \

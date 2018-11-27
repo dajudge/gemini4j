f242929a8b8ff431784eedc3bccf181ee4583d59
Alex Stockinger <mail@alexstockinger.de>
2018-11-27 19:36:56 +0000

    Passing deployment environment variables into docker build

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -5,6 +5,9 @@ echo "CI_PROJECT_DIR: ${CI_PROJECT_DIR}"
 docker build -t builder_jdk8 builder/jdk8
 docker run --rm \
     --net host \
+    -e CI_COMMIT_REF_NAME="$CI_COMMIT_REF_NAME" \
+    -e BINTRAY_API_USER="$BINTRAY_API_USER" \
+    -e BINTRAY_API_KEY="$BINTRAY_API_KEY" \
     -v "${CI_PROJECT_DIR}":/project \
     -v "${CI_PROJECT_DIR}/.cache/.gradle":/root/.gradle \
     -v /var/run/docker.sock:/var/run/docker.sock \
